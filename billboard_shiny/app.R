c7bcddea06b8887f3492bdb3eb4764125b51878f
Katie Cao <katiecao@Katies-MacBook-Pro-6.local>
2019-12-13 18:49:53 -0500

    Added sentiment analysis

--- billboard_shiny/app.R
+++ billboard_shiny/app.R
@@ -7,6 +7,10 @@ timelessness_20 <- read_rds("timelessness_20.rds")
 timeless_words_graphic <- read_rds("timeless_words_graphic.rds")
 diversity_plot <- read_rds("diversity_plot.rds")
 density_plot <- read_rds("density_plot.rds")
+popular_artists <- read_rds("popular_artists.rds")
+sentiment_decades <- read_rds("sentiment_decades.rds")
+lyrics_nrc_sub <- read_rds("lyrics_nrc_sub.rds")
+
 
 ui <- navbarPage(theme = shinytheme("simplex"), "Billboard Top 100 Song Analysis",
                  tabPanel("About"),
@@ -26,31 +30,92 @@ ui <- navbarPage(theme = shinytheme("simplex"), "Billboard Top 100 Song Analysis
                                 weighted by the total number of weeks spent on the top 100 chart. This score accounts 
                                 for each song's weekly rank and the number of weeks spent at those ranks, heavily 
                                 favoring songs that chart for many weeks."),
+                              br(),
                               selectInput("Select one of the top 20 most timeless songs.", "Choose a Song:",
                                           inputId = "song",
                                           choices = unique(top_20$song_label))
                               ),
                             
                             mainPanel(
-                              plotOutput("plot1"),
-                              fluidRow(
-                                align = "center",
-                                h3(textOutput("score")),
-                                width = 12
-                              )
+                              plotOutput("plot1")
                             ))
                   ),
                  tabPanel("Lyrical Analysis",
                           h3("How have lyrics changed over time?", align = "center"),
                           tabsetPanel(
-                            tabPanel("Timeless Words",
-                                    plotOutput("timeless_words")),
-                            tabPanel("Lexical Diversity",
-                                     plotOutput("diversity")),
-                            tabPanel("Lexical Density",
-                                     plotOutput("density"))
-                            )
-                         ),
+                                      tabPanel("Timeless Words",
+                                              br(),
+                                              h4("Some lyrics are persistent over time."),
+                                              br(),
+                                              p("The graphic below shows the 8 most frequently used words in song lyrics 
+                                                for each decade."),
+                                              br(),
+                                              plotOutput("timeless_words")),
+                                    tabPanel("Lexical Diversity",
+                                             br(),
+                                             h4("Lyrics use an increasing diverse vocalubary over time."),
+                                             br(),
+                                             p("The more varied a vocabulary a text possesses, the higher its lexical diversity. 
+                                               Song Vocabulary is a representation of how many unique words are used in a song. 
+                                               This can be shown with a simple graph of the average unique words per song over the years."),
+                                             br(),
+                                             plotOutput("diversity")),
+                                    tabPanel("Repetitiveness",
+                                             br(),
+                                             h4("Lyrics have become more repetitive over time."),
+                                             br(),
+                                             p("Lexical density is the number of unique words divided by the total number of words. 
+                                               This is an indicator of word repetition, which is a critical songwriter's tool."),
+                                             br(),
+                                             plotOutput("density")),
+                                      tabPanel("Sentiment Analysis",
+                                               br(),
+                                               h4("Lyric sentiments has remained relatively the same over time."),
+                                               br(),
+                                             sidebarLayout(
+                                               sidebarPanel(
+                                                 h4("Senti decades"),
+                                                 br(),
+                                                 p("For this project, we'll measure a song's success by looking at the song's timelessness, 
+                                                   or its popularity over time. 
+                                                   The graph to the right shows a song's Billboard Weekly Top 100 rank over time 
+                                                   and its associated timelessness score."),
+                                                 p("The timelessness score is out of 100, with 100 being the most timeless and 0 being 
+                                                   the least timeless. Timelessness is calcuated using the area under the rank over time curve, 
+                                                   weighted by the total number of weeks spent on the top 100 chart. This score accounts 
+                                                   for each song's weekly rank and the number of weeks spent at those ranks, heavily 
+                                                   favoring songs that chart for many weeks.")
+                                                 ),
+                                               
+                                               mainPanel(
+                                                 plotOutput("decades_nrc"))
+                                                 ),
+                                             br(),
+                                             h4("Lyric sentiments don't vary great in aggregate. However, they vary greatly by song."),
+                                             br(),
+                                             sidebarLayout(
+                                               sidebarPanel(
+                                                 h4("Senti per"),
+                                                 br(),
+                                                 p("For this project, we'll measure a song's success by looking at the song's timelessness, 
+                                                   or its popularity over time. 
+                                                   The graph to the right shows a song's Billboard Weekly Top 100 rank over time 
+                                                   and its associated timelessness score."),
+                                                 p("The timelessness score is out of 100, with 100 being the most timeless and 0 being 
+                                                   the least timeless. Timelessness is calcuated using the area under the rank over time curve, 
+                                                   weighted by the total number of weeks spent on the top 100 chart. This score accounts 
+                                                   for each song's weekly rank and the number of weeks spent at those ranks, heavily 
+                                                   favoring songs that chart for many weeks."),
+                                                 br(),
+                                                 selectInput("Select one of the top 20 most popular Artists.", "Choose an Artist:",
+                                                             inputId = "artist",
+                                                             choices = popular_artists)
+                                                 ),
+                                     
+                                     mainPanel(
+                                       plotOutput("artist_nrc"))
+                                     )))
+                          ),
                  tabPanel("Predicting Timelessness")
               )
 
@@ -87,6 +152,27 @@ server <- function(input, output) {
    output$density <- renderPlot({
      density_plot
    })
+   
+   output$decades_nrc <- renderPlot({
+     sentiment_decades
+   })
+   
+   output$artist_nrc <- renderPlot({
+     lyrics_nrc_sub %>%
+       filter(artist == input$artist) %>%
+       count(title, sentiment, decade) %>%
+       mutate(sentiment = reorder(sentiment, n), title = reorder(title, n)) %>%
+       ggplot(aes(sentiment, n, fill = sentiment)) +
+       geom_col() +
+       facet_wrap(~title, ncol = 3, scales = "free") +
+       theme_lyrics() +
+       theme(panel.grid.major.x = element_blank(),
+             axis.text.x = element_blank()) +
+       labs(x = NULL, y = NULL) +
+       ggtitle("NRC Sentiment Song Analysis") +
+       coord_flip()
+     
+   }, height = 750)
 }
 
 # Run the application 
