3b1bd880c3e2ac6a93ad81544397574ad721a1ed
Katie Cao <katiecao@Katies-MacBook-Pro-6.local>
2019-12-13 16:00:36 -0500

    Finished lyrical analysis

--- billboard_shiny/app.R
+++ billboard_shiny/app.R
@@ -1,38 +1,91 @@
 library(shiny)
+library(shinythemes)
+library(scales)
 library(tidyverse)
-top_15 <- write_rds(top_15, "top_15.rds")
+top_20 <- read_rds("top_20.rds")
+timelessness_20 <- read_rds("timelessness_20.rds")
+timeless_words_graphic <- read_rds("timeless_words_graphic.rds")
+diversity_plot <- read_rds("diversity_plot.rds")
+density_plot <- read_rds("density_plot.rds")
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        selectInput("selection", "Choose a Song:",
-                    inputId = "song",
-                    choices = unique(top_15$song_label))
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("plot1")
-      )
-   )
-)
+ui <- navbarPage(theme = shinytheme("simplex"), "Billboard Top 100 Song Analysis",
+                 tabPanel("About"),
+                 tabPanel("Timelessness",
+                          h3("How can we measure a song's success?", align = "center"),
+                          br(),
+                          sidebarLayout(
+                            sidebarPanel(
+                              h4("What is Timelessness?"),
+                              br(),
+                              p("For this project, we'll measure a song's success by looking at the song's timelessness, 
+                              or its popularity over time. 
+                              The graph to the right shows a song's Billboard Weekly Top 100 rank over time 
+                              and its associated timelessness score."),
+                              p("The timelessness score is out of 100, with 100 being the most timeless and 0 being 
+                                the least timeless. Timelessness is calcuated using the area under the rank over time curve, 
+                                weighted by the total number of weeks spent on the top 100 chart. This score accounts 
+                                for each song's weekly rank and the number of weeks spent at those ranks, heavily 
+                                favoring songs that chart for many weeks."),
+                              selectInput("Select one of the top 20 most timeless songs.", "Choose a Song:",
+                                          inputId = "song",
+                                          choices = unique(top_20$song_label))
+                              ),
+                            
+                            mainPanel(
+                              plotOutput("plot1"),
+                              fluidRow(
+                                align = "center",
+                                h3(textOutput("score")),
+                                width = 12
+                              )
+                            ))
+                  ),
+                 tabPanel("Lyrical Analysis",
+                          h3("How have lyrics changed over time?", align = "center"),
+                          tabsetPanel(
+                            tabPanel("Timeless Words",
+                                    plotOutput("timeless_words")),
+                            tabPanel("Lexical Diversity",
+                                     plotOutput("diversity")),
+                            tabPanel("Lexical Density",
+                                     plotOutput("density"))
+                            )
+                         ),
+                 tabPanel("Predicting Timelessness")
+              )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    output$plot1 <- renderPlot({
      ## the input should be the song pulled from top_15 df
      # graph weekly song rank over time for the inputted song
-     top_15 %>%
+     top_20 <- read_rds("top_20.rds")
+     top_20 %>%
        filter(song_label == input$song) %>%
        ggplot(aes(x = date, y = Week.Position)) +
        geom_line() +
        scale_y_reverse()
+   })
+   output$score <- renderText({
+     timelessness_20 <- read_rds("timelessness_20.rds")
+     score <- timelessness_20 %>%
+       filter(song_label == input$song) %>%
+       mutate(timelessness_score_rounded = round(timelessness_score, digits = 2)) %>%
+       pull(timelessness_score_rounded)
+     
+     paste("Timelessness Score:", score)
+   })
+   
+   output$timeless_words <- renderPlot({
+     timeless_words_graphic
+   })
+   
+   output$diversity <- renderPlot({
+     diversity_plot
+   })
+   
+   output$density <- renderPlot({
+     density_plot
    })
 }
 
--- graphics.R
+++ graphics.R
@@ -4,47 +4,57 @@ library(lubridate)
 library(gridExtra) #viewing multiple plots together
 library(tidytext) #text mining
 library(wordcloud2) #creative visualizations
+library(scales)
+library(billboard)
+library(mgcv)
+library(MASS)
 library(tidyverse)
 
-# from https://data.world/kcmillersean/billboard-hot-100-1958-2017
+# Billboard data source: https://data.world/kcmillersean/billboard-hot-100-1958-2017
 
-GET("https://query.data.world/s/mulps2bjunbt2g476gsrgwqjib5dnb", write_disk(tf <- tempfile(fileext = ".xlsx")))
-features <- read_excel(tf)
-
-# load in billboard weekly rank data
+# Load in billboard weekly top 100 songs rank data
 
 billboard <- read.csv("https://query.data.world/s/tpw4liiutfaztvftwockqqhu44yssi", header=TRUE, stringsAsFactors=FALSE) %>%
   mutate(date = mdy(WeekID), song_label = paste(Song, Performer, sep = " by "))
 
-# create a timelessness score based on number of weeks spent in weekly top 100 and the rank.
+# Load in spotify features of songs data
+
+GET("https://query.data.world/s/mulps2bjunbt2g476gsrgwqjib5dnb", write_disk(tf <- tempfile(fileext = ".xlsx")))
+features <- read_excel(tf)
+
+
+# Create a timelessness score based on number of weeks spent in weekly top 100 and the rank.
 # A natural way to score a single’s success on the charts would be to take an integral to find the area
 # under the curves we’ve drawn.  I decided only to count areas where the single was in the top 10.
 # In the below graph, the area of the region above the horizontal line would be found to give the score
 # (more precisely, I took the sum of max(0, 11-chart rank) for each single over all weeks):
 
 timelessness <- billboard %>%
-  filter(Week.Position < 11) %>%
   group_by(song_label, SongID) %>%
-  summarize(timelessness_score = sum(11 - Week.Position)) %>%
+  summarize(timelessness_score_raw = sum((101 - Week.Position)*Weeks.on.Chart)) %>%
+  
+  # Scale the score out of 100, a percentile rank
+  
+  mutate(timelessness_score = (timelessness_score_raw/293883)*100) %>%
   arrange(desc(timelessness_score))
 
-# take top 15 timeless songs for graphic
-timelessness_15 <- timelessness %>%
+# Take top 20 timeless songs for graphic
+timelessness_20 <- timelessness %>%
   ungroup() %>%
-  slice(1:15)
+  slice(1:20)
 
-# top 15 data
-top_15 <- timelessness_15 %>%
+# Add in the timelessness score to the billboard rank data for the top 20 songs
+top_20 <- timelessness_20 %>%
   inner_join(billboard, by = "song_label")
-
-
-write_rds(top_15, "billboard_shiny/top_15.rds")
 
 # join timelessness score data with song feature data by songID
 data <- timelessness %>%
-  inner_join(features, by = "SongID")
+  inner_join(billboard, by = "SongID") %>%
+  inner_join(features, by = "SongID") %>%
+  rename(song_label = song_label.x)
 
-#can i print out the timelessness score..?
+# The lyrics dataset is only 1960-2016, so the dataset will be limited to those.
+#Lyrics from songs on Billboards Hot 100 from 1960 to 2016
 
 # Lyric analysis using NLP and Machine Learning
 
@@ -73,44 +83,135 @@ lyrics_clean$lyrics <- sapply(lyrics_clean$lyrics, fix.contractions)
 
 # function to remove special characters
 removeSpecialChars <- function(x) gsub("[^a-zA-Z0-9 ]", " ", x)
+
 # remove special characters
 lyrics_clean$lyrics <- sapply(lyrics_clean$lyrics, removeSpecialChars)
+
+#make lower case
 lyrics_clean$lyrics <- sapply(lyrics_clean$lyrics, tolower)
 
-# now for the text mining/analytics
+#create the decade column
+lyrics_clean <- lyrics_clean %>%
+  mutate(decade = 
+           ifelse(year %in% 1960:1969, "1960s",
+             ifelse(year %in% 1970:1979, "1970s", 
+                    ifelse(year %in% 1980:1989, "1980s", 
+                           ifelse(year %in% 1990:1999, "1990s", 
+                                  ifelse(year %in% 2000:2009, "2000s", 
+                                         ifelse(year %in% 2010:2016, "2010s", 
+                                                "NA"))))))) %>%
+  mutate(song_label = paste(title, artist, sep = " by ")) %>%
+  mutate(year = as.numeric(year))
+
+lyrics_data <- lyrics_clean %>%
+  inner_join(timelessness, by = "song_label")
+
+# lyrics data only has 2708 variables
+
+# now for the text mining/analytics, this is using the lyrics data alone, without looking at rank, so far
+# we will look at changes over time
+
 # we want to measure lexical complexity
 # we will break out individual lyrics using tokenization
-head(sample(stop_words$word, 15), 15)
-undesirable_words <- c("chorus", "repeat", "lyrics", 
+
+undesirable_words <- c("chorus", "repeat", "lyrics", "hook", "intro", "pre",
                        "theres", "bridge", "fe0f", "yeah", "baby", 
                        "alright", "wanna", "gonna", "chorus", "verse", 
-                       "whoa", "gotta", "make", "miscellaneous", "2", 
-                       "4", "ooh", "uurh", "pheromone", "poompoom", "3121", 
-                       "matic", " ai ", " ca ", " la ", "hey", " na ", 
+                       "whoa", "gotta", "make", "miscellaneous", "1", "2", 
+                       "3", "4", "ooh", "uurh", "pheromone", "poompoom", "3121", 
+                       "matic", " ai ","ai", "outro", " ca ", " la ", "hey", " na ", 
                        " da ", " uh ", " tin ", "  ll", "transcription",
                        "repeats")
-#unnest and remove stop, undesirable
+#unnest and remove stop, undesirable words
 lyrics_filtered <- lyrics_clean %>%
   unnest_tokens(word, lyrics) %>%
   anti_join(stop_words) %>%
   distinct() %>%
   filter(!word %in% undesirable_words)
 
-#maybe have the frequently used words by decade? have a bar plot and a wordcloud? timeless words?
+my_colors <- c("#E69F00", "#56B4E9", "#009E73", "#CC79A7", "#D55E00")
+theme_lyrics <- function() 
+{
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.ticks = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position = "none")
+}
+# Frequently used words over time
+
+timeless_words <- lyrics_filtered %>% 
+  filter(decade != 'NA') %>%
+  group_by(decade) %>%
+  count(word, decade, sort = TRUE) %>%
+  slice(seq_len(8)) %>%
+  ungroup() %>%
+  arrange(decade,n) %>%
+  mutate(row = row_number()) 
+
+timeless_words_graphic <- timeless_words %>%
+  ggplot(aes(row, n, fill = decade)) +
+  geom_col(show.legend = FALSE) +
+  labs(x = NULL, y = "Song Count") +
+  ggtitle("Timeless Words") + 
+  theme_lyrics() +  
+  facet_wrap(~decade, scales = "free", ncol = 6) +
+  scale_x_continuous(  # This handles replacement of row 
+    breaks = timeless_words$row, # notice need to reuse data frame
+    labels = timeless_words$word) +
+  coord_flip()
 
 #lexical density by time and then timelessness by lexical density
 
+lyrics_clean %>%
+  count(artist) %>%
+  arrange(desc(n))
+
 lex_diversity_per_year <- lyrics_clean %>%
   unnest_tokens(word, lyrics) %>%
-  group_by(title,year) %>%
+  group_by(song_label, year) %>%
   summarise(lex_diversity = n_distinct(word)) %>%
-  arrange(desc(lex_diversity)) 
+  arrange(desc(lex_diversity))
+
+diversity_plot <- lex_diversity_per_year %>%
+  ggplot(aes(year, lex_diversity)) +
+  geom_point(color = my_colors[3],
+             alpha = .4, 
+             size = 1, 
+             position = "jitter") + 
+  stat_smooth(formula = y ~ x, color = "black", se = FALSE, method = "lm") +
+  geom_smooth(color = "blue") +
+  ggtitle("Lexical Diversity") +
+  xlab("Year") + 
+  ylab("Lexical Diversity") +
+  scale_color_manual(values = my_colors) +
+  theme_classic() + 
+  theme_lyrics()
+
+diversity_plot
 
 lex_density_per_year <- lyrics_clean %>%
   unnest_tokens(word, lyrics) %>%
-  group_by(title,year) %>%
+  group_by(song_label,year) %>%
   summarise(lex_density = n_distinct(word)/n()) %>%
   arrange(desc(lex_density))
+
+density_plot <- lex_density_per_year %>%
+  ggplot(aes(year, lex_density)) +
+  geom_point(color = my_colors[3],
+             alpha = .4, 
+             size = 1, 
+             position = "jitter") + 
+  stat_smooth(formula = y ~ x, color = "black", se = FALSE, method = "lm") +
+  geom_smooth(color = "blue") +
+  ggtitle("Lexical Density") +
+  xlab("Year") + 
+  ylab("Lexical Density") +
+  scale_color_manual(values = my_colors) +
+  theme_classic() + 
+  theme_lyrics()
+
+density_plot
 
 data <- data %>%
   inner_join(lex_density_per_year, by = c("Song" = "title"))
@@ -134,3 +235,12 @@ data %>%
   ggplot(aes(x = danceability, y = timelessness_score)) +
   geom_point() +
   geom_smooth(method = "glm", formula = timelessness_score ~ danceability)
+
+
+
+
+write_rds(top_20, "billboard_shiny/top_20.rds")
+write_rds(timelessness_20, "billboard_shiny/timelessness_20.rds")
+write_rds(timeless_words_graphic, "billboard_shiny/timeless_words_graphic.rds")
+write_rds(diversity_plot, "billboard_shiny/diversity_plot.rds")
+write_rds(density_plot, "billboard_shiny/density_plot.rds")
